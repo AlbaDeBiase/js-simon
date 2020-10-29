37c32bb829d239eb1ce1de36e33ec1997c86699d
Alba De biase <debiasealba@gmail.com>
2020-10-29 16:58:34 +0100

    imposto il timer di 30 secondi

--- main.js
+++ main.js
@@ -7,20 +7,13 @@
 // BONUS: visualizzare in pagina anche un timer con il countdown dei 30 secondi
 
 
-// visualizare 5 numeri casuali
+// visualizzare 5 numeri casuali
 
-
-// funzione che genera N numeri random, dove N è espresso nel parametro "quantita"
-// riceve in input la quantità di numeri da generare e il limite inferiore e il limite superiore
-// restituisce un array con gli N numeri random generati
 function getRandomNumber(min, max) {
     var number = Math.floor(Math.random() * (max - min + 1) ) + min;
     return number;
 }
 
-// funzione che genera N numeri random, dove N è espresso nel parametro "quantita"
-// riceve in input la quantità di numeri da generare e il limite inferiore e il limite superiore
-// restituisce un array con gli N numeri random generati
 function getRandomNumbers(quantita, min, max) {
     var numeri = [];
     for (var i = 0; i < quantita; i++) {
@@ -30,6 +23,16 @@ function getRandomNumbers(quantita, min, max) {
     return numeri;
 }
 
-// invoco la funzione per ottenere 100 numeri random compresi tra 1 e 1000
+// uso la funzione per ottenere 5 numeri random compresi tra 1 e 100
 var numeri_random = getRandomNumbers(5, 1, 100);
 console.log(numeri_random);
+
+// Da lì parte un timer di 30 secondi.
+setInterval(function () {
+// alert('Hello');
+}, 3000);
+
+// Dopo 30 secondi, vengono rimossi i numeri dalla pagina
+
+ // e l'utente deve inserire (tramite prompt)
+ // i numeri che ha visto precedentemente, uno alla volta.
